bf46da46830e9e3f94c68095643b3ad6fb0affd4
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-03 17:27:05 -0700

    Include IC solution for Reverse String in Place

--- 1-array-and-string-manipulation/reverse-string-in-place.py
+++ 1-array-and-string-manipulation/reverse-string-in-place.py
@@ -15,3 +15,17 @@ def reverse(letters):
     return letters
 
 print(reverse(letters))
+
+
+# IC Solution: 
+
+def reverse(list_of_chars):
+    left_index = 0
+    right_index = len(list_of_chars) - 1
+
+    while left_index < right_index:
+        # Swap characters
+        list_of_chars[left_index], list_of_chars[right_index] = list_of_chars[right_index], list_of_chars[left_index]
+        # Move towards middle
+        left_index += 1
+        right_index -= 1
