845e191e60f567005cf24759a6260bf1ee9f98a0
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-07 15:20:43 -0700

    Include IC solution for Inflight Entertainment

--- 2-hashing-and-hash-tables/inflight-entertainment.py
+++ 2-hashing-and-hash-tables/inflight-entertainment.py
@@ -3,6 +3,8 @@
 # and returns a boolean indicating whether there are two numbers 
 # in movie_lengths whose sum equals flight_length.
 
+
+# My Solution
 def inflight_movie(flight_length, movie_lengths): 
 
     movie_dict = {}
@@ -24,5 +26,18 @@ def inflight_movie(flight_length, movie_lengths):
     return False
 
 
+# IC Solution
 
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+    # Movie lengths we've seen so far
+    movie_lengths_seen = set()
+
+    for first_movie_length in movie_lengths:
+        matching_second_movie_length = flight_length - first_movie_length
+        if matching_second_movie_length in movie_lengths_seen:
+            return True
+        movie_lengths_seen.add(first_movie_length)
+
+    # We never found a match, so return False
+    return False
 
