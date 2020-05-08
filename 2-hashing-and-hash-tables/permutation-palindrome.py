323af28c26e5a052a451fabb323ce3bd423b8a27
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-08 14:22:42 -0700

    Include IC solution for Permutation Palindrome

--- 2-hashing-and-hash-tables/permutation-palindrome.py
+++ 2-hashing-and-hash-tables/permutation-palindrome.py
@@ -25,3 +25,21 @@ def is_palindrome(input_string):
         if odd_count > 1:
             return False
     return True
+ 
+
+ # IC Solution
+
+def has_palindrome_permutation(the_string):
+    # Track characters we've seen an odd number of times
+    unpaired_characters = set()
+
+    for char in the_string:
+        if char in unpaired_characters:
+            unpaired_characters.remove(char)
+        
+        else:
+            unpaired_characters.add(char)
+    
+    # The string has a palindrome permutation if it
+    # has one or zero characters without a pair
+    return len(unpaired_characters) <= 1
