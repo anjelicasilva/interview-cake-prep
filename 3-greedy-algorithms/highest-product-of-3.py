6b2ba29bf5eb1c5aefc1296be4939922b47a813f
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-13 16:24:22 -0700

    Include IC solution for Highest Product of 3

--- 3-greedy-algorithms/highest-product-of-3.py
+++ 3-greedy-algorithms/highest-product-of-3.py
@@ -23,3 +23,37 @@ def highest_product_of_3(list_of_ints):
         elif num > third:
             third = num
     return first * second * num
+
+
+# IC Solution
+def highest_product_of_3(list_of_ints):
+    if len(list_of_ints) < 3:
+        raise ValueError('Less than 3 items!')
+
+    highest = max(list_of_ints[0], list_of_ints[1])
+    lowest = min(list_of_ints[0], list_of_ints[1])
+    highest_product_of_2 = list_of_ints[0] * list_of_ints[1]
+    lowest_product_of_2 = list_of_ints[0] * list_of_ints[1]
+
+    highest_product_of_3 = list_of_ints[0] * list_of_ints[1] * list_of_ints[2]
+
+    for i in range(2, len(list_of_ints)):
+        current = list_of_ints[i]
+
+        highest_product_of_3 = max(highest_product_of_3,
+                                current * highest_product_of_2,
+                                current * lowest_product_of_2)
+
+        highest_product_of_2 = max(highest_product_of_2,
+                                current * highest,
+                                current * lowest)
+
+        lowest_product_of_2 = min(lowest_product_of_2,
+                                current * highest,
+                                current * lowest)
+
+        highest = max(current, highest)
+
+        lowest = min(current, lowest)
+
+    return highest_product_of_3
