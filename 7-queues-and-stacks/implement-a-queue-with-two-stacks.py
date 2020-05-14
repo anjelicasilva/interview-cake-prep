0929a058d6dfa9f7089f94a413a1229eb0e44877
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-14 15:55:21 -0700

    Include IC solution for Implement a Queue with Two Stacks

--- 7-queues-and-stacks/implement-a-queue-with-two-stacks.py
+++ 7-queues-and-stacks/implement-a-queue-with-two-stacks.py
@@ -18,3 +18,23 @@ class QueueTwoStacks(object):
                 add_item = self.in_stack.pop()
                 self.out_stack.append(add_item)
         return add_item
+
+
+# IC Solution
+
+class QueueTwoStacks(object):
+
+    # Implement the enqueue and dequeue methods
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack =[]
+
+    def enqueue(self, item):
+        self.in_stack.append(item)
+
+    def dequeue(self):
+        if len(self.out_stack) == 0:
+            while len(self.in_stack) > 0:
+                add_item = self.in_stack.pop()
+                self.out_stack.append(add_item)
+        return self.out_stack.pop()
