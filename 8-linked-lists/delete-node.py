8e6088912f0f42685d26fcb2b2bf121ce4675ef2
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-15 16:35:30 -0700

    Include IC solution for Delete Node

--- 8-linked-lists/delete-node.py
+++ 8-linked-lists/delete-node.py
@@ -2,7 +2,7 @@
 
 # My Attempt
 
-  class LinkedListNode(object):
+class LinkedListNode(object):
 
     def __init__(self, value):
         self.value = value
@@ -15,3 +15,19 @@
             if current.next == item:
                 current.next = current.next.next
             current = current.next
+
+
+# IC Solution
+
+def delete_node(node_to_delete):
+
+    # Delete the input node from the linked list
+    
+    next_node = node_to_delete.next
+
+    if next_node:
+        node_to_delete.value = next_node.value
+        node_to_delete.next = next_node.next
+    else:
+        raise Exception("Can't delete the last node with this technique")
+
