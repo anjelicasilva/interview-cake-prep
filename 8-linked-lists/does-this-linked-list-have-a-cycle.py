375268ba5ebea9460de528ce14d293c0f295f167
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-16 22:02:01 -0700

    Start problem Does This Linked List Have A Cycle

---
+++ 8-linked-lists/does-this-linked-list-have-a-cycle.py
@@ -0,0 +1,8 @@
+# Write a function contains_cycle() that takes the first node in a singly-linked list and returns a boolean indicating whether the list contains a cycle.
+
+def contains_cycle(first_node):
+
+    # Check if the linked list contains a cycle
+    
+
+    return False
