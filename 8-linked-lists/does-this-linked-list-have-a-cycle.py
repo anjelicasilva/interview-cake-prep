3892418ed2f9cabaef1dc8a72c1c188dd7dce580
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-18 22:57:23 -0700

    Practice linked lists problem: Does This Linked List Have a Cycle using python

--- 8-linked-lists/does-this-linked-list-have-a-cycle.py
+++ 8-linked-lists/does-this-linked-list-have-a-cycle.py
@@ -1,8 +1,15 @@
 # Write a function contains_cycle() that takes the first node in a singly-linked list and returns a boolean indicating whether the list contains a cycle.
 
+# My Attempt
+
 def contains_cycle(first_node):
 
-    # Check if the linked list contains a cycle
-    
+    current = first_node
+
+    while current.next:
+        current.value = "visited"
+        if current.next.value == "visited":
+            return True
+        current = current.next
 
     return False
