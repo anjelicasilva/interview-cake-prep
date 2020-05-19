8bc7731ae221964617c2ce425693fb4dde80300d
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-19 11:24:32 -0700

    Include IC solution for Reverse a Linked List problem

--- 8-linked-lists/reverse-a-linked-list.py
+++ 8-linked-lists/reverse-a-linked-list.py
@@ -24,3 +24,26 @@ def reverse(head_of_list):
         current = curent.next
         current.next = previous
     return
+
+
+    # IC Solution
+
+def reverse(head_of_list):
+    current_node = head_of_list
+    previous_node = None
+    next_node = None
+
+    # Until we have 'fallen off' the end of the list
+    while current_node:
+        # Copy a pointer to the next element
+        # before we overwrite current_node.next
+        next_node = current_node.next
+
+        # Reverse the 'next' pointer
+        current_node.next = previous_node
+
+        # Step forward in the list
+        previous_node = current_node
+        current_node = next_node
+
+    return previous_node
