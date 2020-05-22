04ebbdd3590e88885cf83b1ab56bd1504db217b2
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-21 20:38:50 -0700

    Start linked list problem: kth to last node in a singly linked list

---
+++ 8-linked-lists/kth-to-last-node-in-a-singly-linked-list.py
@@ -0,0 +1,24 @@
+# Write a function kth_to_last_node() that takes an integer kk and the head_node of a singly-linked list, and returns the kkth to last node in the list.
+
+# For example:
+
+#   class LinkedListNode:
+
+#     def __init__(self, value):
+#         self.value = value
+#         self.next  = None
+
+
+# a = LinkedListNode("Angel Food")
+# b = LinkedListNode("Bundt")
+# c = LinkedListNode("Cheese")
+# d = LinkedListNode("Devil's Food")
+# e = LinkedListNode("Eccles")
+
+# a.next = b
+# b.next = c
+# c.next = d
+# d.next = e
+
+## Returns the node with value "Devil's Food" (the 2nd to last node)
+# kth_to_last_node(2, a)
