20e4c81918e3bc71a942906321c3743e30fc07d4
Anjelica Silva <anjelicacsilva@gmail.com>
2020-05-22 23:27:46 -0700

    Include IC first approach solution for Kth to Last Node in a Singly-Linked List

--- 8-linked-lists/kth-to-last-node-in-a-singly-linked-list.py
+++ 8-linked-lists/kth-to-last-node-in-a-singly-linked-list.py
@@ -22,3 +22,43 @@
 
 ## Returns the node with value "Devil's Food" (the 2nd to last node)
 # kth_to_last_node(2, a)
+
+
+
+
+# IC Solution: First Approach
+
+def kth_to_last_node(k, head):
+    if k < 1:
+        raise ValueError(
+            'Impossible to find less than first to last node: %s' % k
+        )
+
+    # Step 1: get the length of the list
+    # Start at 1, not 0
+    # else we'd fail to count the head node!
+    list_length = 1
+    current_node = head
+
+    # Traverse the whole list,
+    # counting all the nodes
+    while current_node.next:
+        current_node = current_node.next
+        list_length += 1
+
+    # If k is greater than the length of the list, there can't
+    # be a kth-to-last node, so we'll return an error!
+    if k > list_length:
+        raise ValueError(
+            'k is larger than the length of the linked list: %s' % k
+        )
+
+    # Step 2: walk to the target node
+    # Calculate how far to go, from the head,
+    # to get to the kth to last node
+    how_far_to_go = list_length - k
+    current_node = head
+    for i in range(how_far_to_go):
+        current_node = current_node.next
+
+    return current_node
