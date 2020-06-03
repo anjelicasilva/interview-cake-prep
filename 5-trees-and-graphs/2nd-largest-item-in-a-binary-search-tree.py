a0c4a941b7262271947b8b6027186f110a0ff4cd
Anjelica Silva <anjelicacsilva@gmail.com>
2020-06-02 20:13:26 -0700

    Practice trees and graphs problem: 2nd Largest Item in a Binary Search Tree using python

--- 5-trees-and-graphs/2nd-largest-item-in-a-binary-search-tree.py
+++ 5-trees-and-graphs/2nd-largest-item-in-a-binary-search-tree.py
@@ -16,3 +16,30 @@
 #     def insert_right(self, value):
 #         self.right = BinaryTreeNode(value)
 #         return self.right
+
+
+# My Attempt with hints
+
+def find_largest(root_node):
+    if root_node is None:
+        raise ValueError('Tree must have at least 1 node')
+        
+    if root_node.right is not None:
+        return find_largest(root_node.right)
+    return root_node.value
+
+
+def find_second_largest(root_node):
+
+    # Find the second largest item in the binary search tree
+    
+    if (root_node is None or (root_node.left is None and root_node.right is None)):
+        raise ValueError('Tree must have at least 2 nodes')
+        
+    if root_node.left and not root_node.right:
+        return find_largest(root_node.left)
+        
+    if (root_node.right and not root_node.right.right and not root_node.right.left):
+        return root_node.value
+        
+    return find_second_largest(root_node.right)
