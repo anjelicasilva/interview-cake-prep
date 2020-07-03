6c688c98cc0a6d76337a99304dcca1150545efda
Anjelica Silva <anjelicacsilva@gmail.com>
2020-07-02 23:37:21 -0700

    Create first function to walk through the list and use a set to keep track of which items we've seen

--- 4-sorting-searching-and-logarithms/find-repeat-space-edition.py
+++ 4-sorting-searching-and-logarithms/find-repeat-space-edition.py
@@ -1,1 +1,12 @@
 # Write a function which finds an integer that appears more than once in our list. (If there are multiple duplicates, you only need to find one of them.)
+
+def find_repeat(numbers):
+    numbers_seen = set()
+    for number in numbers:
+        if number in numbers_seen:
+            return number
+        else:
+            numbers_seen.add(number)
+
+    # Whoops--no duplicate
+    raise Exception('no duplicate!')
