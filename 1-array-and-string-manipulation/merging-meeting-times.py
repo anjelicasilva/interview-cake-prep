67c0e29765b9b8f5676fc0c383afef34e0c62fee
Anjelica Silva <anjelicacsilva@gmail.com>
2020-07-23 19:47:11 -0700

    Review Array and String Manipulation problem: Merging Meeting Times and include Interview Cake solution

--- 1-array-and-string-manipulation/merging-meeting-times.py
+++ 1-array-and-string-manipulation/merging-meeting-times.py
@@ -1,3 +1,9 @@
+# Write a solution that's efficient even when we can't put a nice upper bound on the numbers representing our time ranges. Here we've simplified our times down to the number of 30-minute slots past 9:00 am. But we want the method to work even for very large numbers, like Unix timestamps. In any case, the spirit of the challenge is to merge meetings where startTime and endTime don't have an upper bound.
+
+
+
+
+
 def merge_ranges(meetings):
     new_list = []
     merged_meetings = set()
@@ -21,3 +27,30 @@ def merge_ranges(meetings):
     return new_list
 
 print(merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]))  
+
+
+
+# IC Solution
+
+def merge_ranges(meetings):
+
+    # Sort by start time
+    sorted_meetings = sorted(meetings)
+
+    # Initialize merged_meetings with the earliest meeting
+    merged_meetings = [sorted_meetings[0]]
+
+    for current_meeting_start, current_meeting_end in sorted_meetings[1:]:
+        last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
+
+        # If the current meeting overlaps with the last merged meeting, use the
+        # later end time of the two
+        if (current_meeting_start <= last_merged_meeting_end):
+            merged_meetings[-1] = (last_merged_meeting_start,
+                                   max(last_merged_meeting_end,
+                                       current_meeting_end))
+        else:
+            # Add the current meeting since it doesn't overlap
+            merged_meetings.append((current_meeting_start, current_meeting_end))
+
+    return merged_meetings
